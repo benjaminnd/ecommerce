0e815094270adca665d872dab242b828101f35ae
Minh <minhnhatdang1111@gmail.com>
2020-12-03 22:39:03 -0500

    Modified start scripts for deployment

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -9,6 +9,7 @@ import UserAuth from '../middleware/auth.js';
 import async from 'async'
 import Product from '../models/Product.js';
 
+console.log(expressValidator['default'])
 const UserRouter = express.Router();
 const check = expressValidator.check;
 const validationResult = expressValidator.validationResult;
