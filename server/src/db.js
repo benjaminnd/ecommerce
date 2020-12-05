938505f3d705bc22c652755316178c4ee558f5bf
Minh <minhnhatdang1111@gmail.com>
2020-12-04 23:15:40 -0500

    Change dependencies for production

--- server/src/db.js
+++ server/src/db.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose';
 
-const WithDb = async() => {
-    const connect = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    });
-    console.log(`MongoDB connected: ${connect.connection.host}`)
-
+try {
+    const WithDb = async() => {
+        const connect = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB connected: ${connect.connection.host}`)
+    
+    }
+  
+} catch (error) {
+    console.error(`Error: ${error}`)
+    process.exit(1)
 }
-
 export default WithDb;
