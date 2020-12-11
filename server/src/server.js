29f72acb2abfbb69e46ab38a632f741cb42d9178
Minh <minhnhatdang1111@gmail.com>
2020-12-11 15:32:56 -0500

    Change server port

--- server/src/server.js
+++ server/src/server.js
@@ -43,7 +43,7 @@ app.use((req, res)=>{
     })
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`)
